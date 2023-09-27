930b4bd052d292bb8dd39b6f2842f1df336b0b74
Wiktoria <wiktoria123sr@gmail.com>
2023-09-27 18:54:52 +0200

    Add ConnectionString to appsettings

---
+++ simple-repository-API/Models/DBConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace simple_repository_API.Models
+{
+    public class DBConfiguration
+    {
+        public static string ConnectionString { get; set; }
+    }
+}
